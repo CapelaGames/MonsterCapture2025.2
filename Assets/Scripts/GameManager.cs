bb963b9778698d73a4e2a4a6f6569db040ff8727
Capela <andrew@Capelagames.com.au>
2025-11-05 15:59:53 +1100

    high score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private GameObject player;
+    void Start()
+    {
+        player = FindFirstObjectByType<PlayerMovement>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < -10)
+        {
+            ScoreManager.instance.AddHighScore("Player", ScoreManager.instance.currentScore);
+            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,5 +1,14 @@
+using System;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+
+[Serializable]
+public struct ScoreData
+{
+    public string name;
+    public int score;
+}
 
 public class ScoreManager : MonoBehaviour
 {
@@ -7,6 +16,14 @@ public class ScoreManager : MonoBehaviour
 
     public TMP_Text scoreText;
     public int currentScore = 0;
+
+    public List<ScoreData> scores = new List<ScoreData>();
+    public int maxScores = 10;
+
+    public TMP_Text scorePrefab;
+    public Transform scoreParent;
+
+
 
     private void Awake()
     {
@@ -23,5 +40,109 @@ public class ScoreManager : MonoBehaviour
     public void UpdateGUI()
     {
         scoreText.text = "Score: " + currentScore;
+    }
+
+    [ContextMenu("Update High Score")]
+    public void UpdateHighScores()
+    {
+        DestroyAllChildren(scoreParent);
+        foreach (var score in scores)
+        {
+            TMP_Text text = Instantiate(scorePrefab, scoreParent);
+            text.text = score.name + ": " + score.score;
+        }
+    }
+
+    private void DestroyAllChildren(Transform parent)
+    {
+        Transform[] children = parent.GetComponentsInChildren<Transform>(true);
+        for (int i = children.Length -1; i >=0 ; i --)
+        {
+            if (children[i] == parent.transform) continue;
+            if (Application.isPlaying)
+            {
+                Destroy(children[i].gameObject);
+            }
+            else
+            {
+                DestroyImmediate(children[i].gameObject);
+            }
+            
+        }
+    }
+
+    bool isDisplaying = false;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (isDisplaying)
+            {
+                DestroyAllChildren(scoreParent);
+            }
+            else
+            {
+                UpdateHighScores();
+            }
+            isDisplaying = !isDisplaying;
+        }
+    }
+
+    [ContextMenu("Add Random Score")]
+    public void AddHighScore()
+    {
+        int randomScore = UnityEngine.Random.Range(0, 1000);
+        AddHighScore(randomScore);
+    }
+
+    // overloaded methods
+    public void AddHighScore(int score)
+    {
+        string[] possible = new[] { "AAA", "BBB", "CCC", "DDD", "EEE", "FFF", "GGG", "HHH", "III", "JJJ" };
+        string randomName = possible[UnityEngine.Random.Range(0, possible.Length)];
+        AddHighScore(randomName, score);
+
+       // Array.Sort(possible);
+
+    }
+
+    // overloaded methods
+    public void AddHighScore(string name, int score)
+    {
+        UpdateHighScores();
+
+        if (scores.Count == 0)
+        {
+            scores.Add(new ScoreData() { name = name, score = score });
+            return;
+        }
+
+        for (int i = 0; i < scores.Count; i++)
+        {
+            if (score > scores[i].score)
+            {
+                scores.Insert(i, new ScoreData() { name = name, score = score });
+                if (scores.Count > maxScores)
+                {
+                    scores.RemoveAt(scores.Count - 1);
+                }
+                return;
+            }
+        }
+
+        if (scores.Count < maxScores)
+        {
+            scores.Add(new ScoreData() { name = name, score = score });
+        }
+
+        RemoveScoresPastMax();
+    }
+
+    void RemoveScoresPastMax()
+    {
+        for(int i = scores.Count - 1; i >= maxScores; i--)
+        {
+            scores.RemoveAt(i);
+        }
     }
 }
